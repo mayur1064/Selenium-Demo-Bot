7ec31a7867788643c65b1784d3789eeecb20ac56
Mayur Bhor <mayurtrap@gmail.com>
2022-11-20 23:40:18 +0530

    Download files logic added

--- src/main/java/com/example/demo/scheduler/ScheduledTask.java
+++ src/main/java/com/example/demo/scheduler/ScheduledTask.java
@@ -31,6 +31,10 @@ public class ScheduledTask {
     @Value("${driverpath}")
     private String webDriverPath;
 
+    @Value("${downloadpath}")
+    private String downloadPath;
+
+
     static final Logger LOGGER = LoggerFactory.getLogger(ScheduledTask.class);
 
     @Scheduled(cron = "*/30 * * * * ?")
@@ -45,29 +49,36 @@ public class ScheduledTask {
 
         ChromeDriverService driverService = ChromeDriverService.createDefaultService();
         ChromeDriver driver = new ChromeDriver(driverService, options);
-//
-//        Map<String, Object> commandParams = new HashMap<>();
-//        commandParams.put("cmd", "Page.setDownloadBehavior");
-//        Map<String, String> params = new HashMap<>();
-//        params.put("behavior", "allow");
-//        params.put("downloadPath", "E:\\EROS\\si-bot\\");
-//        commandParams.put("params", params);
-//        ObjectMapper objectMapper = new ObjectMapper();
-//        HttpClient httpClient = HttpClientBuilder.create().build();
-//        String command = objectMapper.writeValueAsString(commandParams);
-//        String u = driverService.getUrl().toString() + "/session/" + driver.getSessionId() + "/chromium/send_command";
-//        HttpPost request = new HttpPost(u);
-//        request.addHeader("content-type", "application/json");
-//        request.setEntity(new StringEntity(command));
-//        httpClient.execute(request);
-        LOGGER.info(((RemoteWebDriver)driver).getCapabilities().toString());
-        driver.get("https://www.omgeo.com/login");
+
+        Map<String, Object> commandParams = new HashMap<>();
+        commandParams.put("cmd", "Page.setDownloadBehavior");
+        Map<String, String> params = new HashMap<>();
+        params.put("behavior", "allow");
+        params.put("downloadPath", downloadPath);
+        commandParams.put("params", params);
+        ObjectMapper objectMapper = new ObjectMapper();
+        HttpClient httpClient = HttpClientBuilder.create().build();
+        String command = objectMapper.writeValueAsString(commandParams);
+        String u = driverService.getUrl().toString() + "/session/" + driver.getSessionId() + "/chromium/send_command";
+        HttpPost request = new HttpPost(u);
+        request.addHeader("content-type", "application/json");
+        request.setEntity(new StringEntity(command));
+        httpClient.execute(request);
+        LOGGER.info("Opening selenium website");
+        driver.get("http://www.seleniumhq.org/download/");
+        LOGGER.info("PageTitle " + driver.getTitle());
         LOGGER.info("PageSource " + driver.getPageSource());
-//        driver.get("http://www.seleniumhq.org/download/");
-//        driver.findElement(By.linkText("32 bit Windows IE")).click();
+        driver.findElement(By.linkText("32 bit Windows IE")).click();
         LOGGER.info("File downloaded successfully");
+//
+
+
+//        driver.get("https://www.omgeo.com/login");
+
 
 
     }
 
 }
+
+//LOGGER.info(((RemoteWebDriver)driver).getCapabilities().toString());
