3ff933f01de44c9877be0d6687c28143d97e70f7
Mayur Bhor <mayurtrap@gmail.com>
2022-11-20 22:21:11 +0530

    Added changes for fedoraa

--- src/main/java/com/example/demo/scheduler/ScheduledTask.java
+++ src/main/java/com/example/demo/scheduler/ScheduledTask.java
@@ -12,8 +12,10 @@ import org.openqa.selenium.chrome.ChromeDriverService;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.CapabilityType;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import org.testcontainers.shaded.com.fasterxml.jackson.databind.ObjectMapper;
@@ -26,35 +28,43 @@ import java.util.concurrent.TimeUnit;
 @Component
 public class ScheduledTask {
 
+    @Value("${driverpath}")
+    private String webDriverPath;
+
     static final Logger LOGGER = LoggerFactory.getLogger(ScheduledTask.class);
 
-    @Scheduled(cron = "*/20 * * * * ?")
+    @Scheduled(cron = "*/30 * * * * ?")
     public void executeOpenBrowser() throws IOException, InterruptedException {
-        System.setProperty("webdriver.chrome.driver","C:\\UBS\\Dev\\EROS\\ssi-bot\\chromedriver.exe");
+//        System.setProperty("webdriver.chrome.driver","E:\\EROS\\chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver",webDriverPath);
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--test-type");
         options.addArguments("--headless");
         options.addArguments("--disable-extensions"); //to disable browser extension popup
+        options.addArguments("--disable-gpu");
 
         ChromeDriverService driverService = ChromeDriverService.createDefaultService();
         ChromeDriver driver = new ChromeDriver(driverService, options);
-
-        Map<String, Object> commandParams = new HashMap<>();
-        commandParams.put("cmd", "Page.setDownloadBehavior");
-        Map<String, String> params = new HashMap<>();
-        params.put("behavior", "allow");
-        params.put("downloadPath", "C:\\UBS\\Dev\\EROS\\ssi-bot\\");
-        commandParams.put("params", params);
-        ObjectMapper objectMapper = new ObjectMapper();
-        HttpClient httpClient = HttpClientBuilder.create().build();
-        String command = objectMapper.writeValueAsString(commandParams);
-        String u = driverService.getUrl().toString() + "/session/" + driver.getSessionId() + "/chromium/send_command";
-        HttpPost request = new HttpPost(u);
-        request.addHeader("content-type", "application/json");
-        request.setEntity(new StringEntity(command));
-        httpClient.execute(request);
-        driver.get("http://www.seleniumhq.org/download/");
-        driver.findElement(By.linkText("32 bit Windows IE")).click();
+//
+//        Map<String, Object> commandParams = new HashMap<>();
+//        commandParams.put("cmd", "Page.setDownloadBehavior");
+//        Map<String, String> params = new HashMap<>();
+//        params.put("behavior", "allow");
+//        params.put("downloadPath", "E:\\EROS\\si-bot\\");
+//        commandParams.put("params", params);
+//        ObjectMapper objectMapper = new ObjectMapper();
+//        HttpClient httpClient = HttpClientBuilder.create().build();
+//        String command = objectMapper.writeValueAsString(commandParams);
+//        String u = driverService.getUrl().toString() + "/session/" + driver.getSessionId() + "/chromium/send_command";
+//        HttpPost request = new HttpPost(u);
+//        request.addHeader("content-type", "application/json");
+//        request.setEntity(new StringEntity(command));
+//        httpClient.execute(request);
+        LOGGER.info(((RemoteWebDriver)driver).getCapabilities().toString());
+        driver.get("https://www.omgeo.com/login");
+        LOGGER.info("PageSource " + driver.getPageSource());
+//        driver.get("http://www.seleniumhq.org/download/");
+//        driver.findElement(By.linkText("32 bit Windows IE")).click();
         LOGGER.info("File downloaded successfully");
 
 
